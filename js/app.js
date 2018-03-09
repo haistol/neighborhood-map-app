7240df67c0b52e687d26d46b344be715176b0e74
Juan De la Rosa <juan.d@futurelinkcorporation.com>
2018-03-09 16:04:08 -0400

    separate the view model from the map view model

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ var MapLocation= {
     center: {lat: 40.7413549, lng: -73.99802439999996},
         zoom: 13
 }
-var ListMarkers= [{lat: 40.719526, lng: -74.0089934}];
-var ViewModel = function(){
+var ListMarkers= [{name:"cool place",location:{lat: 40.719526, lng: -74.0089934}}];
+var MapViewModel = function(){
 
     map = new google.maps.Map(document.getElementById('map'),MapLocation);
     var markerOptions = {
-        position: ListMarkers[0],
+        position: ListMarkers[0].location,
         animation: google.maps.Animation.DROP,
       };
   
@@ -16,4 +16,8 @@ var ViewModel = function(){
     marker.setMap(map);
 }
 
+var ViewModel = function(){
+    var self = this;
+}
 
+ko.applyBindings(new ViewModel());
